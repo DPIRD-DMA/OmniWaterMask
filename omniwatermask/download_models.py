469668c82b08fa1746c692829987ac076e43889e
Nick <wrignj08@gmail.com>
2025-08-21 10:52:09 +0800

    Fixed model download location

--- omniwatermask/download_models.py
+++ omniwatermask/download_models.py
@@ -1,11 +1,15 @@
+from importlib import resources
 from pathlib import Path
 from typing import Union
 
 import gdown
 import pandas as pd
+import platformdirs
 import torch
 from huggingface_hub import hf_hub_download
 from safetensors.torch import load_file
+
+from .__version__ import __version__ as omniwatermask_version
 
 
 def download_file_from_google_drive(file_id: str, destination: Path) -> None:
@@ -51,10 +55,21 @@ def download_file(file_id: str, destination: Path, source: str) -> None:
         )
 
 
+def get_model_data_dir() -> Path:
+    """Get the user data directory for model files"""
+    data_dir = Path(
+        platformdirs.user_data_dir(
+            "omniwatermask", version=omniwatermask_version, ensure_exists=True
+        )
+    )
+    return data_dir
+
+
 def get_models(
     force_download: bool = False,
     model_dir: Union[str, Path, None] = None,
     source: str = "hugging_face",
+    model_version: float = 1.0,
 ) -> list[dict]:
     """
     Downloads the model weights from Google Drive and saves them locally.
@@ -65,18 +80,28 @@ def get_models(
         source (str): The source from which the model weights should be downloaded. Currently, only "google_drive" or "hugging_face" are supported.
     """
 
-    df = pd.read_csv(
-        Path(__file__).resolve().parent / "models/model_download_links.csv"
-    )
+    with (resources.files("omniwatermask") / "model_download_links.csv").open() as f:
+        model_df = pd.read_csv(f)
+
+    model_df["version"] = model_df["version"].astype(float)
+    available_versions = model_df["version"].unique()
+    if model_version not in available_versions:
+        raise ValueError(
+            f"""Model version {model_version} not found. 
+            Available versions: {available_versions}"""
+        )
+    # filter models by version
+    model_df = model_df[model_df["version"] == model_version]
+
     model_paths = []
 
-    for _, row in df.iterrows():
-        file_id = str(row["google_drive_id"])
+    if model_dir is not None:
+        model_dir = Path(model_dir)
+    else:
+        model_dir = get_model_data_dir()
 
-        if model_dir is not None:
-            model_dir = Path(model_dir)
-        else:
-            model_dir = Path(__file__).resolve().parent / "models"
+    for _, row in model_df.iterrows():
+        file_id = str(row["google_drive_id"])
 
         model_dir.mkdir(exist_ok=True)
         destination = model_dir / str(row["file_name"])
